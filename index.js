831870e026d3075c9d2102dd9367f2639b9d9e0b
batiot <batiot@gmail.com>
2017-10-24 23:05:08 +0200

    replace mdns lib for a full js one

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ let ip = require("ip");
 let path = require('path');
 let pm2GUI = require('pm2-gui');
 let pm2 = require('pm2');
-let mdns = require('mdns');
+var bonjour = require('bonjour')();
 const { spawn } = require('child_process');
 
 
@@ -17,7 +17,7 @@ const ADMIN_HTTP_PORT = 8081;
 pm2.connect(function (err) {
   if (err) {
     console.error(err);
-    process.exit(2);
+    //process.exit(2);
   }
   console.log("pm2 connected");
   /*
@@ -32,9 +32,6 @@ process.on('SIGINT', function() {
   console.log('stop')
 });
 
-
-var imServices = {};
-
 //#########################
 //for serving html files
 //#########################
@@ -42,14 +39,14 @@ var imServices = {};
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname + '/public/assets')));
 app.get('/', function (req, res, next) {
-  res.render('admin',imServices);
+  res.render('admin',getServices());
 });
 app.get('/simulator', function (req, res, next) {
-  res.render('simulator',imServices);
+  res.render('simulator',getServices());
 });
 app.get('/services', function (req, res, next) {
   res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify(imServices));
+  res.send(JSON.stringify(getServices()));
 });
 app.get('/halt', function (req, res, next) {
   res.setHeader('Content-Type', 'text/plain');
@@ -80,110 +77,44 @@ app.get('/init3', function (req, res, next) {
 });
 
 
-
 server.listen(ADMIN_HTTP_PORT);
 console.log('\x1b[35m%s\x1b[0m', "admin web server is up on http://" + ip.address() + ":" + ADMIN_HTTP_PORT);
-let webAdminServiceType = mdns.makeServiceType({ name: 'im-admin', protocol: 'tcp' });
-let adWebAdmin = mdns.createAdvertisement(webAdminServiceType, ADMIN_HTTP_PORT);
-adWebAdmin.start();
-
-discoverService('im-web');
-discoverService('im-admin');
-discoverService('im-broker');
-discoverService('im-pm2');
+bonjour.publish({ name: 'admin', type: 'im-admin',subtypes:["im","admin"], port: ADMIN_HTTP_PORT });
 
 
-//##############################################################################
-//#########################   CPU & MEME     ###################################
-//##############################################################################
-/*
+var browser = bonjour.find();
+browser.on("up",(service)=>{
+  console.log('up service:', service)
+});
+browser.on("down",(service)=>{
+  //console.log('down service:', service)
+});
+//discoverService('googlecast');
 
-var stat = require('./stat')
-setInterval(runTimer, self.options.process_refresh)
-function runTimer () {
-  pidusage.stat(pid, function (err, stat) {
-    if (err) {
-      delete self._cache.usages[pidStr]
-      return self._emitError(err, {
-        id: pid,
-        namespace: conf.NSP.PROCESS
-      })
-    }
-    stat.memory = stat.memory * 100 / totalmem
+function getServices(){
+  let imServices={};
+  browser.services
+  .filter((service)=>service.type.indexOf('im')==0)
+  .map((service)=>{
 
-    var data = {
-      pid: pid,
-      time: Date.now(),
-      usage: stat
-    }
-    mqtt.publish
-  })
-*/
-
+    let ipV4address = (service.addresses[1] &&  service.addresses[1].length< service.addresses[0].length ) ? service.addresses[1] : service.addresses[0];
+    if(!ipV4address){ipV4address = service.referer.address;}
+    //transition from type to bonjour name
+    let name = service.type.indexOf('im-')==0? service.type.substr(3):service.type;
+    console.log(name,service);
+    imServices[name] = { address: ipV4address, host:  service.host, port: service.port, txtRecord: service.txt };
+  });
+  return imServices;
+}
 
 
 //##############################################################################
 //#########################   PM2 GUI        ###################################
 //##############################################################################
 console.log("pm2-gui starting");
-let pm2GuiServiceType = mdns.makeServiceType({ name: 'im-pm2', protocol: 'tcp' });
-let adPm2Gui = mdns.createAdvertisement(pm2GuiServiceType, 8088);
-adPm2Gui.start();
+bonjour.publish({ name: 'pm2', type: 'im-pm2',subtypes:["im","pm2"], port: 8088 });
 pm2GUI.startWebServer('./pm2-gui.ini');//port 8088
 //pm2GUI.startAgent('./pm2-gui.ini');
 //pm2GUI.dashboard('./pm2-gui.ini');
 console.log("pm2-gui started");
 //##############################################################################
-
-
-
-
-function discoverService(serviceName) {
-  let sequence = [
-    mdns.rst.DNSServiceResolve()
-    , 'DNSServiceGetAddrInfo' in mdns.dns_sd ? mdns.rst.DNSServiceGetAddrInfo() : mdns.rst.getaddrinfo({ families: [4] }) //IPV4
-    , mdns.rst.makeAddressesUnique()
-  ];
-  let browser = mdns.createBrowser(mdns.tcp(serviceName), { resolverSequence: sequence });
-  browser.on('serviceUp', function (service) {
-    //ignore vm interface and no im mdns
-    if (service.networkInterface.indexOf('docker') > -1
-      || service.networkInterface.indexOf('virb') > -1
-      || service.type.name.indexOf('im') != 0
-    ) return;
-    // ignore duplicate ups
-    if (imServices[service.type.name]) return;
-
-    /*
-  service up:  { interfaceIndex: 3,
-    type:
-     ServiceType {
-       name: 'im-admin',
-       protocol: 'tcp',
-       subtypes: [],
-       fullyQualified: true },
-    replyDomain: 'local.',
-    flags: 2,
-    name: 'dbuntu',
-    networkInterface: 'wlp1s0',
-    fullname: 'dbuntu._im-admin._tcp.local.',
-    host: 'dbuntu.local.',
-    port: 8081,
-    addresses: [ '192.168.122.1' ] }
-    */
-    imServices[service.type.name] = { addresses: service.addresses[0], host:  service.name, port: service.port, networkInterface: service.networkInterface, txtRecord: service.txtRecord };
-    console.log("service up: ", service.type.name);
-  });
-  browser.on('serviceDown', function (service) {
-    //ignore vm interface and no im service
-    if (service.networkInterface.indexOf('docker') > -1
-      || service.networkInterface.indexOf('virb') > -1
-      || service.type.name.indexOf('im') != 0
-    ) return;
-    // ignore duplicate downs
-    if (!imServices[service.type.name]) return;
-    delete imServices[service.type.name];
-    console.log("service down: ", service.type.name);
-  });
-  browser.start();
-}
